def86428adbcef55deba0d5c8f6747f370a15b46
wintercamo <wintercamo3482@gmail.com>
2023-12-14 21:30:15 +0900

    update_Dec_14_3

--- tk_src/field_test_with_QT.py
+++ tk_src/field_test_with_QT.py
@@ -9,6 +9,48 @@ import cv2, imutils
 import time, datetime
 from ultralytics import YOLO
 import mysql.connector
+import boto3
+from botocore.exceptions import NoCredentialsError
+
+# S3 클라이언트 초기화
+def initialize_s3_client():
+    try:
+        s3_client = boto3.client('s3')
+        return s3_client
+    except NoCredentialsError:
+        print("Credentials not available")
+        return None
+
+# S3 버킷에 파일 업로드
+def upload_file_to_s3(file_name, bucket, object_name=None):
+    s3_client = initialize_s3_client()
+    if s3_client is None:
+        return False
+
+    if object_name is None:
+        object_name = file_name
+
+    try:
+        s3_client.upload_file(file_name, bucket, object_name)
+        print(f"File {file_name} uploaded to {bucket}/{object_name}")
+        return True
+    except NoCredentialsError:
+        print("Error in uploading file")
+        return False
+
+# S3 버킷에서 파일 다운로드
+def download_file_from_s3(bucket, object_name, file_name):
+    s3_client = initialize_s3_client()
+    if s3_client is None:
+        return False
+
+    try:
+        s3_client.download_file(bucket, object_name, file_name)
+        print(f"File {file_name} downloaded from {bucket}/{object_name}")
+        return True
+    except NoCredentialsError:
+        print("Error in downloading file")
+        return False
 
 # aws RDS 연결
 aws_DB = mysql.connector.connect(
@@ -47,31 +89,32 @@ sql_query = f'''CREATE TABLE {my_table_name} (
                 X_max float,
                 y_min float,
                 y_max float,
-                box_num int,
-                link VARCHAR(255)
+                link_picture VARCHAR(255),
+                link_video VARCHAR(255)
             )'''
 
 my_cursor.execute(f"SHOW TABLES LIKE %s", (my_table_name,))  # 만드려는 테이블명이 중복인지 아닌지 확인
 
+# 테이블 없으면 생성
 if my_cursor.fetchone() is None:
     my_cursor.execute(sql_query)
     aws_DB.commit()
     print(f"테이블 '{my_table_name}'를 생성했습니다.")
+
+# 있으면 스킵
 else:
     print(f"테이블 '{my_table_name}'가 이미 존재합니다. 생성을 스킵합니다.")
 
 # INSERT 쿼리 불러오기
-sql_query = f'''INSERT INTO {my_table_name} (time, class, x_min, y_min, x_max, y_max, link)
-                                     VALUES (%s, %s, %s, %s, %s, %s, %s)'''
+sql_query = f'''INSERT INTO {my_table_name} (time, class, x_min, y_min, x_max, y_max, link_picture, link_video)
+                                     VALUES (%s, %s, %s, %s, %s, %s, %s, %s)'''
 
 # 경계박스관련 변수
 CONFIDENCE_THRESHOLD = 0.8
 RED = (255, 0, 0)
-GREEN = (0, 255, 0)
 WHITE = (255, 255, 255)
 
 # 식별 모델
-# model = YOLO('/home/wintercamo/dev_ws/Project_ML/src/runs/detect/train3/weights/best.pt')
 model = YOLO('/home/wintercamo/dev_ws/Project_ML/src/best.pt')
 
 # 감지 객체 명칭
@@ -79,10 +122,6 @@ mycoco = open('/home/wintercamo/dev_ws/Project_ML/src/mycoco.txt', 'r')
 data = mycoco.read()
 class_list = data.split('\n')
 mycoco.close()
-
-# 파일 경로
-video_path = "/home/wintercamo/dev_ws/Project_ML/data/videos"
-image_path = "/home/wintercamo/dev_ws/Project_ML/data/images"
 
 # 카메라 클래스
 class Camera(QThread):
@@ -96,10 +135,7 @@ class Camera(QThread):
             self.update.emit()
             time.sleep(0.1)
 
-class RecordViewer(QThread):
-    pass
-
-# UI 소환
+# UI 불러오기
 from_class = uic.loadUiType("/home/wintercamo/dev_ws/Project_ML/src/myui.ui")[0]
 
 class WindowClass(QMainWindow, from_class):
@@ -110,7 +146,7 @@ class WindowClass(QMainWindow, from_class):
 
         # 실시간 영상 화면
         self.image = None
-        self.video = cv2.VideoCapture('/home/wintercamo/dev_ws/Project_ML/data/samples/wild_boars.mp4')
+        self.video = cv2.VideoCapture('/home/wintercamo/dev_ws/Project_ML/data/samples/deer.mp4')  # 웹캠 or 로컬 영상 파일
         self.camera = Camera()
         self.realtime_display = QPixmap()
         self.camera.start()
@@ -126,16 +162,81 @@ class WindowClass(QMainWindow, from_class):
         self.record_stop_timer = QTimer(self)
         self.record_stop_timer.timeout.connect(self.recordingStop)
 
-        self.past_second = datetime.datetime.now().second
-        self.current_second = datetime.datetime.now().second
+        self.past_second = 0
 
-    def captureImage(self):        
-        timing = datetime.datetime.now().strftime('%y_%m_%d_%H_%M_%S')
-        filename = timing + '.jpg'
+        self.upload_stack_video = []
+        self.upload_stack_images = []
 
-        full_path = os.path.join(image_path, filename)
+#     def openFile(self):
+#         file = QFileDialog.getOpenFileName(filter='Image (*.*)')
+
+#         image = cv2.imread(file[0])
+#         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+#         h,w,c = image.shape
+#         qimage = QImage(image.data, w, h, w*c, QImage.Format_RGB888)
+#         self.pixmap = self.pixmap.fromImage(qimage)
+#         self.pixmap = self.pixmap.scaled(self.display.width(), self.display.height())
+#         self.display.setPixmap(self.pixmap)
+
+    def record_on_DB(self, detected_class, xmin, ymin, xmax, ymax):
+        self.row = self.info_table.rowCount()
+        self.info_table.insertRow(self.row)
+
+        self.info_table.setItem(self.row, 0, QTableWidgetItem(str(self.now)))
+        self.info_table.setItem(self.row, 1, QTableWidgetItem(detected_class))
+
+        self.info_table.setItem(self.row, 2, QTableWidgetItem(str(xmin)))
+        self.info_table.setItem(self.row, 3, QTableWidgetItem(str(ymin)))
+        self.info_table.setItem(self.row, 4, QTableWidgetItem(str(xmax)))
+        self.info_table.setItem(self.row, 5, QTableWidgetItem(str(ymax)))
+
+        self.info_table.setItem(self.row, 6, QTableWidgetItem(str(self.image_full_path_s3)))
+        self.info_table.setItem(self.row, 7, QTableWidgetItem(str(self.video_full_path_s3)))
+    
+        my_cursor.execute(sql_query, (str(self.now), detected_class, str(xmin), str(ymin), str(xmax),str(ymax),
+                                      str(self.image_full_path_s3), str(self.video_full_path_s3)))
+        aws_DB.commit()
+    
+    def captureImage(self):
+        timing = self.now
+        filename = timing.replace(' ', '').replace('/','').replace(':','').replace('_','')
+        link = "images/" + filename[:6]
+
+        filename = filename + ".jpg"
+        self.image_full_path_s3 = os.path.join(link, filename)
+        self.image_full_path_local = os.path.join("/home/wintercamo/dev_ws/Project_ML/data/images", filename)
         image = cv2.cvtColor(self.image, cv2.COLOR_BGR2RGB)
-        cv2.imwrite(full_path, image)
+        cv2.imwrite(self.image_full_path_local, image)
+        upload_file_to_s3(self.image_full_path_local, 'prj-wildlife', self.image_full_path_s3)
+        # self.upload_stack_images.append((self.image_full_path_local, self.image_full_path_s3))
+        
+    def updateRecording(self):
+        self.writer.write(cv2.cvtColor(self.image, cv2.COLOR_RGB2BGR))
+
+    def recordingStop(self):
+        if self.record_flag == True:
+            self.record_flag = False
+            self.writer.release()
+            upload_file_to_s3(self.video_full_path_local, 'prj-wildlife', self.video_full_path_s3)
+            # self.upload_stack_video.append((self.video_full_path_local, self.video_full_path_s3))
+
+    def recordingStart(self):
+        self.record.start()
+
+        timing = self.now
+        filename = timing.replace(' ', '').replace('/','').replace(':','').replace('_','')
+        link = "videos/" + filename[:6]
+        
+        filename = filename + '.avi'
+        self.video_full_path_s3 = os.path.join(link, filename)
+        self.video_full_path_local = os.path.join("/home/wintercamo/dev_ws/Project_ML/data/videos", filename)
+        self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    
+        w = int(self.video.get(cv2.CAP_PROP_FRAME_WIDTH))
+        h = int(self.video.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    
+        self.writer = cv2.VideoWriter(self.video_full_path_local, self.fourcc, 20.0, (w, h))
+        self.record_stop_timer.start(10000)  # 30초: 30000
 
     def detect_target(self):
         detection = model(self.image)[0]
@@ -156,66 +257,13 @@ class WindowClass(QMainWindow, from_class):
             cv2.rectangle(self.image, (xmin, ymin), (xmax, ymax), RED, 2)
             cv2.putText(self.image, class_list[label]+' '+str(round(confidence, 2)), (xmin, ymin), cv2.FONT_ITALIC, 1, WHITE, 2)
             
+            self.current_second = int(self.now[-2:])
+
             if (self.current_second - self.past_second) >= 2:
                 self.captureImage()
-            else:
-                self.past_second = self.current_second
-
-            self.record_on_DB(class_list[label], xmin, ymin, xmax, ymax)
-
-        self.current_second = datetime.datetime.now().second
-
-    def record_on_DB(self, detected_class, xmin, ymin, xmax, ymax):
-        self.row = self.info_table.rowCount()
-        self.info_table.insertRow(self.row)
-
-        timing = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-
-        self.info_table.setItem(self.row, 0, QTableWidgetItem(str(timing)))
-        self.info_table.setItem(self.row, 1, QTableWidgetItem(detected_class))
-
-        self.info_table.setItem(self.row, 2, QTableWidgetItem(str(xmin)))
-        self.info_table.setItem(self.row, 3, QTableWidgetItem(str(ymin)))
-        self.info_table.setItem(self.row, 4, QTableWidgetItem(str(xmax)))
-        self.info_table.setItem(self.row, 5, QTableWidgetItem(str(ymax)))
-        
-        filename = timing[2:]
-        filename = filename.replace(' ', '_').replace('-','_').replace(':','_')
-        filename = video_path + '/' +filename + '.avi'
-        self.info_table.setItem(self.row, 6, QTableWidgetItem(str(filename)))
-    
-        my_cursor.execute(sql_query, (str(timing), detected_class, str(xmin), str(ymin), str(xmax), str(ymax), str(filename)))
-        aws_DB.commit()
-    
-    def recordingStop(self):
-        if self.record_flag == True:
-            self.record_flag = False
-            self.writer.release()
-
-    def recordingStart(self):
-        self.record.start()
-
-        timing = datetime.datetime.now().strftime('%y_%m_%d_%H_%M_%S')
-        filename = timing + '.avi'
-        full_path = os.path.join(video_path, filename)
-        self.fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    
-        w = int(self.video.get(cv2.CAP_PROP_FRAME_WIDTH))
-        h = int(self.video.get(cv2.CAP_PROP_FRAME_HEIGHT))
-    
-        self.writer = cv2.VideoWriter(full_path, self.fourcc, 20.0, (w, h))
-        self.record_stop_timer.start(30000)  # 30초: 30000
-
-    def openFile(self):
-        file = QFileDialog.getOpenFileName(filter='Image (*.*)')
-
-        image = cv2.imread(file[0])
-        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        h,w,c = image.shape
-        qimage = QImage(image.data, w, h, w*c, QImage.Format_RGB888)
-        self.pixmap = self.pixmap.fromImage(qimage)
-        self.pixmap = self.pixmap.scaled(self.display.width(), self.display.height())
-        self.display.setPixmap(self.pixmap)
+                self.record_on_DB(class_list[label], xmin, ymin, xmax, ymax)
+            
+            self.past_second = self.current_second
 
     def updateCamera(self):
         retval, self.image = self.video.read()
@@ -237,10 +285,14 @@ class WindowClass(QMainWindow, from_class):
     def closeEvent(self, event):  # gui를 닫는 즉시 실행되는 함수
         if self.record_flag:
             self.recordingStop()
+        
+        # for _ in range(len(self.upload_stack_video)):
+
+            
+        # self.upload_stack_images
+        # upload_file_to_s3(self.image_full_path_local, 'prj-wildlife', self.image_full_path_s3)
+        # upload_file_to_s3(self.video_full_path_local, 'prj-wildlife', self.video_full_path_s3)
         aws_DB.close()
-    
-    def updateRecording(self):
-        self.writer.write(cv2.cvtColor(self.image, cv2.COLOR_RGB2BGR))
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
